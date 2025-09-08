f5d44b6ad54711ff7d3eea79702f000c98cf6f8e
jerrsapps1 <Jerr.hernandez1@outlook.com>
2025-09-08 18:51:16 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Features from "./pages/Features";
+import Pricing from "./pages/Pricing";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Resources from "./pages/Resources";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-1">
-        <Outlet />
+    <BrowserRouter>
+      <header className="p-4 border-b">
+        <nav className="max-w-6xl mx-auto flex gap-4">
+          <Link to="/">Home</Link>
+          <Link to="/features">Features</Link>
+          <Link to="/pricing">Pricing</Link>
+          <Link to="/about">About</Link>
+          <Link to="/resources">Resources</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </header>
+
+      <main className="max-w-6xl mx-auto p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </main>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
